f11928eabbaf36611c3c8680a76feeb2af7d17c7
aananev <6167006@gmail.com>
2021-09-06 02:32:26 +0300

    SpringBoot simple BankApplication

---
+++ src/main/java/aananev/example/demoBank/controller/BalanceController.java
@@ -0,0 +1,39 @@
+package aananev.example.demoBank.controller;
+
+import aananev.example.demoBank.model.TransferBalance;
+import aananev.example.demoBank.service.BankService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@RestController("/balance")
+@AllArgsConstructor
+public class BalanceController {
+
+    private final BankService bankService;
+
+    @GetMapping("/{accountId}")
+    public BigDecimal getBalance(@PathVariable Long accountId) {
+        return bankService.getBalance(accountId);
+    }
+
+    @PostMapping ("/add")
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance) {
+        return bankService.addMoney(transferBalance.getTo(), transferBalance.getAmount());
+    }
+
+    @PostMapping("/transfer")
+    public void makeTransfer(@RequestBody TransferBalance transferBalance) {
+        bankService.makeTransfer(transferBalance);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e) {
+      log.error(e.getMessage());
+      return "I'm broken );";
+    }
+
+}
---
+++ src/main/java/aananev/example/demoBank/service/BankService.java
@@ -0,0 +1,48 @@
+package aananev.example.demoBank.service;
+
+import aananev.example.demoBank.model.TransferBalance;
+import aananev.example.demoBank.repo.BalanceRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@AllArgsConstructor
+public class BankService {
+
+    private final BalanceRepository repository;
+
+    public BigDecimal getBalance(Long accountId) {
+        BigDecimal balance = repository.getBalanceForId(accountId);
+        if (balance == null) {
+            throw new IllegalArgumentException();
+        }
+        return balance;
+    }
+
+    public BigDecimal addMoney(Long to, BigDecimal amount) {
+        BigDecimal currentBalance = repository.getBalanceForId(to);
+            if (currentBalance == null) {
+                repository.save(to, amount);
+                return amount;
+            } else {
+                BigDecimal updatedBalance = currentBalance.add(amount);
+                repository.save(to, updatedBalance);
+                return updatedBalance;
+            }
+        }
+
+    public void makeTransfer(TransferBalance transferBalance) {
+        BigDecimal fromBalance = repository.getBalanceForId(transferBalance.getFrom());
+        BigDecimal toBalance = repository.getBalanceForId(transferBalance.getTo());
+        if (fromBalance == null || toBalance == null) throw new IllegalArgumentException("Empty Id");
+        if (transferBalance.getAmount().compareTo(fromBalance) > 0)
+            throw new IllegalArgumentException("You have no founds");
+
+        BigDecimal updatedFromBalance = fromBalance.subtract(transferBalance.getAmount());
+        BigDecimal updatedToBalance = toBalance.add(transferBalance.getAmount());
+        repository.save(transferBalance.getFrom(), updatedFromBalance);
+        repository.save(transferBalance.getTo(), updatedToBalance);
+    }
+}
